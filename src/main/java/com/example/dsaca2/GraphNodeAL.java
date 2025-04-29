66929a7d75be13290e09ba9aacd72cfabfd64fda
Thomas O'Mahony <20103278@mail.wit.ie>
2025-04-29 14:37:29 +0100

    added remaining graph methods

--- src/main/java/com/example/dsaca2/GraphNodeAL.java
+++ src/main/java/com/example/dsaca2/GraphNodeAL.java
@@ -147,6 +147,7 @@ public class GraphNodeAL<T> {
     public class GraphNodeAL2<T> {
 
         public T data;
+        public int nodeValue=Integer.MAX_VALUE;
         public List<GraphLinkAL> adjList = new ArrayList<>(); //Adjacency list now contains link objects = key!
 
         //Could use any concrete List implementation
@@ -183,6 +184,7 @@ public class GraphNodeAL<T> {
                 if (!encountered.contains(adjLink.destNode))
                     traverseGraphDepthFirstShowingTotalCost(adjLink.destNode, encountered, totalCost + adjLink.cost);
         }
+
         //Sort adjacency list of edges based on cost (lowest first)
         // TODO Collections.sort(from.adjList,(a,b)->a.cost-b.cost);
 
@@ -212,8 +214,50 @@ public class GraphNodeAL<T> {
                     allPaths.add(temp); //Add the new candidate path to the list of all costed paths
                 }//If no paths were found then return null. Otherwise, return the cheapest path (i.e. the one with min pathCost)
             return allPaths.isEmpty() ? null : Collections.min(allPaths, (p1,p2)->p1.pathCost-p2.pathCost);
-        }//TODO pg 58 in slides so far
+        }
 
+        public <T> CostedPath findCheapestPathDijkstra(GraphNodeAL2<?> startNode, T lookingfor){
+            CostedPath cp=new CostedPath(); //Create result object for cheapest path
+            List<GraphNodeAL2<?>> encountered=new ArrayList<>(), unencountered=new ArrayList<>(); //Create encountered/unencountered lists
+            startNode.nodeValue=0; //Set the starting node value to zero
+            unencountered.add(startNode); //Add the start node as the only value in the unencountered list to start
+            GraphNodeAL2<?> currentNode;
+            do{ //Loop until unencountered list is empty
+                currentNode=unencountered.remove(0); //Get the first unencountered node (sorted list, so will have lowest value)
+                encountered.add(currentNode); //Record current node in encountered list
+                if(currentNode.data.equals(lookingfor)){ //Found goal - assemble path list back to start and return it
+                    cp.pathList.add(currentNode); //Add the current (goal) node to the result list (only element)
+                    cp.pathCost=currentNode.nodeValue; //The total cheapest path cost is the node value of the current/goal node
+                    while(currentNode!=startNode) { //While we're not back to the start node...
+                        boolean foundPrevPathNode=false; //Use a flag to identify when the previous path node is identified
+                        for(GraphNodeAL2<?> n : encountered) { //For each node in the encountered list...
+                            for(GraphLinkAL e : n.adjList) //For each edge from that node...
+                                if(e.destNode==currentNode && currentNode.nodeValue-e.cost==n.nodeValue){ //If that edge links to the
+                                    //current node and the difference in node values is the cost of the edge -> found path node!
+                                    cp.pathList.add(0,n); //Add the identified path node to the front of the result list
+                                    currentNode=n; //Move the currentNode reference back to the identified path node
+                                    foundPrevPathNode=true; //Set the flag to break the outer loop
+                                    break; //We've found the correct previous path node and moved the currentNode reference
+                                    //back to it so break the inner loop
+                                }
+                            if(foundPrevPathNode) break; //We've identified the previous path node, so break the inner loop to continue
+                        }
+                    }
+                    //Reset the node values for all nodes to (effectively) infinity so we can search again (leave no footprint!)
+                    for(GraphNodeAL2<?> n : encountered) n.nodeValue=Integer.MAX_VALUE;
+                    for(GraphNodeAL2<?> n : unencountered) n.nodeValue=Integer.MAX_VALUE;
+                    return cp; //The costed (cheapest) path has been assembled, so return it!
+                }
+                //We're not at the goal node yet, so...
+                for(GraphLinkAL e : currentNode.adjList) //For each edge/link from the current node...
+                    if(!encountered.contains(e.destNode)) { //If the node it leads to has not yet been encountered (i.e. processed)
+                        e.destNode.nodeValue=Integer.min(e.destNode.nodeValue, currentNode.nodeValue+e.cost); //Update the node value at the end of the edge to the minimum of its current value or the total of the current node's value plus the cost of the edge
+                        if(!unencountered.contains(e.destNode)) unencountered.add(e.destNode);
+                    }
+                Collections.sort(unencountered,(n1,n2)->n1.nodeValue-n2.nodeValue); //Sort in ascending node value order
+            }while(!unencountered.isEmpty());
+            return null; //No path found, so return null
+        }
     }
 }
 
