97f3b9af1b9c6ef50c5f4236136cbee4a783bac8
Thomas O'Mahony <20103278@mail.wit.ie>
2025-05-19 14:21:45 +0100

    fixed fxml

--- src/main/java/com/example/dsaca2/HelloController.java
+++ src/main/java/com/example/dsaca2/HelloController.java
@@ -7,9 +7,14 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
@@ -18,13 +23,25 @@ public class HelloController implements Initializable {
     @FXML
     private ChoiceBox<String> choiceBox;
     @FXML
-    private ImageView imageView;
+    private ImageView mapView;
+
+    private File tempFile;
+
+    private Image detailedMap;
+
+    private Image simpleMap;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Add options to the choiceBox
         choiceBox.getItems().addAll("Fewest Stops", "Shortest Route", "Shortest with fewest changes");
-        imageView.setImage(new Image(getClass().getResource("StationMapWithContent.png").toExternalForm()));
+        detailedMap = new Image(getClass().getResourceAsStream("StationMapWithContent.png"));
+        mapView.setImage(detailedMap);
+    }
+
+    public void loadMap(ActionEvent actionEvent){
+        mapView.setImage(detailedMap);
+        System.out.println("this does work, you are not crazy");
     }
 
     public void choiceBoxOptions(ActionEvent actionEvent) throws IOException {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.dsaca2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.dsaca2 to javafx.fxml;
