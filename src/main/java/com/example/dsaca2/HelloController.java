62bab623cf0b355d8f5d1341475a4c7cd770a9f2
Thomas O'Mahony <20103278@mail.wit.ie>
2025-05-19 15:58:31 +0100

    csv shenanigans

--- src/main/java/com/example/dsaca2/HelloController.java
+++ src/main/java/com/example/dsaca2/HelloController.java
@@ -11,17 +11,21 @@ import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
-import java.util.Objects;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
     @FXML
-    private ChoiceBox<String> choiceBox;
+    private ChoiceBox<String> searchOption;
+    @FXML
+    private ChoiceBox<String> startingStop;
+    @FXML
+    private ChoiceBox<String> endStop;
+    @FXML
+    private ChoiceBox<String> avoidStop;
     @FXML
     private ImageView mapView;
 
@@ -33,8 +37,9 @@ public class HelloController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         // Add options to the choiceBox
-        choiceBox.getItems().addAll("Fewest Stops", "Shortest Route", "Shortest with fewest changes");
+        searchOption.getItems().addAll("Fewest Stops", "Shortest Route", "Shortest with fewest changes");
     }
 
     public void detailedMap(ActionEvent actionEvent) throws IOException {
@@ -48,6 +53,19 @@ public class HelloController implements Initializable {
     }
 
     public void choiceBoxOptions(ActionEvent actionEvent) throws IOException {
+        if (searchOption.getValue().equals("Fewest Stops")) {
+            String startingStop = startingStop.getValue();
+        }
+    }
 
+    public void csvReader() throws IOException {
+        BufferedReader csvReader = new BufferedReader(new FileReader(String.valueOf(getClass().getResource("/data/vienna_subway.csv"))));
+        String line = "";
+
+        while((line = csvReader.readLine()) != null) {
+            System.out.println(line);
+            String[] data = line.split(",");
+            System.out.println("starting destination " + data[0] + "ending destination" + data[1]);
+        }
     }
 }
