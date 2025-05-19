f40220b69800db362755a28ac5c3206cb12a8f24
Thomas O'Mahony <20103278@mail.wit.ie>
2025-05-19 12:50:48 +0100

    Basic FXML + added images

--- src/main/java/com/example/dsaca2/HelloController.java
+++ src/main/java/com/example/dsaca2/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.dsaca2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class HelloController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ChoiceBox<String> choiceBox;
+    @FXML
+    private ImageView imageView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Add options to the choiceBox
+        choiceBox.getItems().addAll("Fewest Stops", "Shortest Route", "Shortest with fewest changes");
+        imageView.setImage(new Image(getClass().getResource("StationMapWithContent.png").toExternalForm()));
+    }
+
+    public void choiceBoxOptions(ActionEvent actionEvent) throws IOException {
+
     }
 }
